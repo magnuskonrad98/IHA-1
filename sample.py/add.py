c87ca1d321474ea7fd6219962ad3e99c33414e08
Your Name <magnuskonrad@gmail.com>
2020-03-23 16:40:12 +0000

    took out unnecessary code

--- sample.py/add.py
+++ sample.py/add.py
@@ -2,9 +2,6 @@ def add(numbers):
     if numbers == "":
         return 0
 
-    elif "," not in numbers:
-        return int(numbers)
-
     else:
         num_list = numbers.split(",")
         num_sum = 0
