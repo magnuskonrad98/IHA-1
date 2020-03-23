221c21ffa90659553271ca836b66084caabf09e5
Your Name <magnuskonrad@gmail.com>
2020-03-23 16:48:03 +0000

    functions now splits on both '\n' and ','

--- sample.py/add_test.py
+++ sample.py/add_test.py
@@ -17,3 +17,9 @@ def test_multiple_integers():
     assert add("1,2,3") == 6
     assert add("2,4,6,8") == 20
     assert add("10,20,30,67,3") == 130
+
+def test_two_delimeters():
+    assert add("1\n2") == 3
+    assert add("2,0\n5\n3,1") == 11
+    assert add("8\n8\n8\n8\n8") == 40
+    
