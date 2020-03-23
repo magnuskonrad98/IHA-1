41cb1f940e4dd679998d65454a7e5069291e7a1e
Your Name <magnuskonrad@gmail.com>
2020-03-23 16:31:41 +0000

    added test for two integers

--- sample.py/add.py
+++ sample.py/add.py
@@ -4,3 +4,7 @@ def add(numbers):
 
     elif "," not in numbers:
         return int(numbers)
+
+    else:
+        num_a, num_b = numbers.split(",")
+        return int(num_a) + int(num_b)
--- sample.py/add_test.py
+++ sample.py/add_test.py
@@ -7,3 +7,8 @@ def test_empty_string_returns_zero():
 def test_single_integer():
     assert add("1") == 1
     assert add("24") == 24
+
+def test_two_integers():
+    assert add("2,4") == 6
+    assert add("10,13") == 23
+    assert add("9,10") == 19
